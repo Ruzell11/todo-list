fe5d02093aab5a5395e23effeb539458384d6f94
Ruzell11 <johnruzell123@gmail.com>
2024-04-25 20:47:42 +0800

    Initialized project

---
+++ target/debug/deps/todo_list.d
@@ -0,0 +1,5 @@
+C:\Users\PC\OneDrive\Desktop\rust\todo-list\target\debug\deps\todo_list.exe: src\main.rs
+
+C:\Users\PC\OneDrive\Desktop\rust\todo-list\target\debug\deps\todo_list.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/todo-list.d
@@ -0,0 +1,1 @@
+C:\Users\PC\OneDrive\Desktop\rust\todo-list\target\debug\todo-list.exe: C:\Users\PC\OneDrive\Desktop\rust\todo-list\src\main.rs
